8a40d338a816fae8698685152848bc0adf20d212
ryosei.kojima <ryosei.8721.soccer@icloud.com>
2019-02-08 02:53:06 +0000

    toppage correction

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,6 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @log = current_user.logs.build
-      @total_min = Log.where(id: current_user.belonged_ids).last.total_min
     end
   end
 end
