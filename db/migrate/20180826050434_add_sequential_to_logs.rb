0ad02f0715168c44072970f87a0c12bed1698dd4
ryosei.kojima <ryosei.8721.soccer@icloud.com>
2019-02-08 02:27:53 +0000

    sequential_id correction

--- db/migrate/20180826050434_add_sequential_to_logs.rb
+++ db/migrate/20180826050434_add_sequential_to_logs.rb
@@ -2,6 +2,5 @@ class AddSequentialToLogs < ActiveRecord::Migration[5.0]
   def change
     add_column :logs, :sequential_id, :integer
   end
-  # 複合ユニーク制約
-  add_index :logs, [:user_id, :sequential_id], unique: true
+  
 end
